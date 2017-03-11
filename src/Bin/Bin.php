825960608a342ccd2da462c2a19c89eec358012a
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-03-11 14:16:06 +0100

    First commit.

---
+++ src/Bin/Bin.php
@@ -0,0 +1,115 @@
+<?php
+
+/**
+ * Hoa
+ *
+ *
+ * @license
+ *
+ * New BSD License
+ *
+ * Copyright © 2007-2017, Hoa community. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Hoa nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace Kitab\Bin;
+
+use Hoa\Console;
+use Hoa\Dispatcher;
+use Hoa\Exception;
+use Hoa\Router;
+
+class Bin
+{
+    public static function main()
+    {
+        self::setErrorHandler();
+
+        try {
+            $router     = self::getRouter();
+            $dispatcher = self::getDispatcher();
+
+            exit((int) $dispatcher->dispatch($router));
+        } catch (Exception $e) {
+            $message = $e->raise(true);
+            $code    = 1;
+        } catch (\Exception $e) {
+            $message = $e->getMessage();
+            $code    = 2;
+        }
+
+        self::exitWith($message, $code);
+    }
+
+    protected static function setErrorHandler(): void
+    {
+        Exception\Error::enableErrorHandler();
+        Exception::enableUncaughtHandler();
+    }
+
+    protected static function getRouter(): Router\Cli
+    {
+        $router = new Router\Cli();
+        $router->get(
+            'g',
+            '(?<command>\w+)?(?<_tail>.*?)',
+            'main',
+            'main',
+            [
+                'command' => 'welcome'
+            ]
+        );
+
+        return $router;
+    }
+
+    protected static function getDispatcher(): Dispatcher\ClassMethod
+    {
+        $dispatcher = new Dispatcher\ClassMethod([
+            'synchronous.call'
+                => 'Kitab\Bin\(:%variables.command:lU:)',
+            'synchronous.able'
+                => 'run'
+        ]);
+        $dispatcher->setKitName(Console\Dispatcher\Kit::class);
+
+        return $dispatcher;
+    }
+
+    protected static function exitWith(string $message, int $code)
+    {
+        ob_start();
+
+        Console\Cursor::colorize('foreground(white) background(red)');
+        echo $message, "\n";
+        Console\Cursor::colorize('normal');
+        $content = ob_get_contents();
+
+        ob_end_clean();
+
+        file_put_contents('php://stderr', $content);
+        exit($code);
+    }
+}
---
+++ src/Compiler/Parser.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * Hoa
+ *
+ *
+ * @license
+ *
+ * New BSD License
+ *
+ * Copyright © 2007-2017, Hoa community. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Hoa nor the names of its contributors may be
+ *       used to endorse or promote products derived from this software without
+ *       specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS AND CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace Kitab\Compiler;
+
+use Hoa\File;
+use Kitab\Exception;
+use PhpParser\Error;
+use PhpParser\NodeTraverser;
+use PhpParser\ParserFactory;
+
+class Parser
+{
+    protected static $_phpParser    = null;
+    protected static $_phpTraverser = null;
+
+    public function __construct()
+    {
+        if (null === self::$_phpParser) {
+            self::$_phpParser = (new ParserFactory())->create(ParserFactory::PREFER_PHP7);
+        }
+
+        if (null === self::$_phpTraverser) {
+            self::$_phpTraverser = new NodeTraverser();
+        }
+    }
+
+    public function parse(File\SplFileInfo $file)
+    {
+        $phpParser = self::getPhpParser();
+        $fileName  = $file->getPathName();
+
+        try {
+            $statements = $phpParser->parse(file_get_contents($fileName));
+        } catch (Error $e) {
+            throw new Exception\PhpParserError(
+                'A syntax error has been found in the file `%s`:' . "\n" .
+                '> %s',
+                0,
+                [$fileName, $e->getMessage()],
+                $e
+            );
+        }
+
+        print_r($this->collectTokens($statements));
+
+        return;
+    }
+
+    protected function collectTokens(array $statements): array
+    {
+        $dataCollector = new Visitor\DataCollector();
+
+        $traverser = self::getTraverser();
+        $traverser->addVisitor($dataCollector);
+
+        $traverser->traverse($statements);
+
+        $traverser->removeVisitor($dataCollector);
+
+        return $dataCollector->getCollection();
+    }
+
+    protected static function getPhpParser()
+    {
+        return self::$_phpParser;
+    }
+
+    protected static function getTraverser()
+    {
+        return self::$_phpTraverser;
+    }
+}
