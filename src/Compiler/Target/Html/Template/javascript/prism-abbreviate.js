6899cfab3e7cd0f7a55e80285ab49348788c218e
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-03-17 19:58:01 +0100

    Target: Add Prism abbreviate extension.

---
+++ src/Compiler/Target/Html/Template/javascript/prism-abbreviate.js
@@ -0,0 +1,35 @@
+(function(){
+    if (
+        typeof self !== 'undefined' && !self.Prism ||
+        typeof global !== 'undefined' && !global.Prism
+    ) {
+        return;
+    }
+
+    Prism.hooks.add('wrap', function(env) {
+        if ('php' !== env.language ||
+            'keyword' !== env.type) {
+            return;
+        }
+
+        console.log(env);
+
+        switch (env.content) {
+            case 'public':
+            case 'protected':
+            case 'private':
+                env.content =
+                    '<abbr title="' + env.content + '">' +
+                    env.content.substring(0, 3) +
+                    '</abbr>';
+
+                break;
+
+            case 'function':
+                env.content = '<abbr title="' + env.content + '">fn</abbr>';
+
+                break;
+            
+        }
+    });
+})();
