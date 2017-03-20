869bcc54e04cccfd7341265be39fb98fe09225bf
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-03-20 15:49:08 +0100

    Prepare the search database as a standalone file.

--- src/Compiler/Target/Html/Template/javascript/application.js
+++ src/Compiler/Target/Html/Template/javascript/application.js
@@ -1,27 +1,3 @@
-window.searchItems = [
-    {
-        id: "foo",
-        name: "finder",
-        description: "hello world",
-        related: "foobar",
-        url: "gordon",
-    },
-    {
-        id: "bar",
-        name: "compiler",
-        description: "test test",
-        related: "baz qux",
-        url: "freeman",
-    },
-    {
-        id: "baz",
-        name: "name",
-        description: "hello world",
-        related: "compiler compiler",
-        url: "gordon",
-    }
-];
-
 document.addEventListener(
     'DOMContentLoaded',
     function () {
---
+++ src/Compiler/Target/Html/Template/javascript/search-database.js
@@ -0,0 +1,23 @@
+window.searchItems = [
+    {
+        id: "foo",
+        name: "finder",
+        description: "hello world",
+        related: "foobar",
+        url: "gordon",
+    },
+    {
+        id: "bar",
+        name: "compiler",
+        description: "test test",
+        related: "baz qux",
+        url: "freeman",
+    },
+    {
+        id: "baz",
+        name: "name",
+        description: "hello world",
+        related: "compiler compiler",
+        url: "gordon",
+    }
+];
