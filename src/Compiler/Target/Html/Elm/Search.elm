39dfa96d5a237c9c101ca3caed51bb24da5adc74
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-04-06 14:02:13 +0200

    Quality: Run `elm-format`.

--- src/Compiler/Target/Html/Elm/Search.elm
+++ src/Compiler/Target/Html/Elm/Search.elm
@@ -7,33 +7,38 @@ import Html.Attributes.Aria exposing (..)
 import Html.Events exposing (..)
 import ElmTextSearch exposing (..)
 
+
 type alias SearchIndex =
-    { id: String
-    , normalizedName: String
-    , description: String
+    { id : String
+    , normalizedName : String
+    , description : String
     }
 
+
 type alias SearchMetadata =
-    { id: String
-    , name: String
-    , description: String
-    , url: String
+    { id : String
+    , name : String
+    , description : String
+    , url : String
     }
+
 
 searchConfiguration =
     { ref = .id
     , fields =
-        [ (.normalizedName, 3.0)
-        , (.description, 1.0)
+        [ ( .normalizedName, 3.0 )
+        , ( .description, 1.0 )
         ]
     , listFields = []
     }
-        
-searchIndex: ElmTextSearch.Index SearchIndex
+
+
+searchIndex : ElmTextSearch.Index SearchIndex
 searchIndex =
     ElmTextSearch.new searchConfiguration
 
-emptySearchMetadata: SearchMetadata
+
+emptySearchMetadata : SearchMetadata
 emptySearchMetadata =
     { id = ""
     , name = "(unknown)"
@@ -41,85 +46,100 @@ emptySearchMetadata =
     , url = ""
     }
 
+
 type alias InitInput =
-    { serializedSearchIndex:  String
-    , searchDatabase: List SearchMetadata
+    { serializedSearchIndex : String
+    , searchDatabase : List SearchMetadata
     }
 
-init: InitInput -> (Model, Cmd Message)
+
+init : InitInput -> ( Model, Cmd Message )
 init input =
-    (
-        { content = ""
-        , searchDatabase = input.searchDatabase
-        , searchIndex = Result.withDefault searchIndex (ElmTextSearch.fromString searchConfiguration input.serializedSearchIndex)
-        }
-        , Cmd.none
+    ( { content = ""
+      , searchDatabase = input.searchDatabase
+      , searchIndex = Result.withDefault searchIndex (ElmTextSearch.fromString searchConfiguration input.serializedSearchIndex)
+      }
+    , Cmd.none
     )
 
+
 type alias Model =
-    { content: String
-    , searchDatabase: List SearchMetadata
-    , searchIndex: ElmTextSearch.Index SearchIndex
+    { content : String
+    , searchDatabase : List SearchMetadata
+    , searchIndex : ElmTextSearch.Index SearchIndex
     }
 
-model: Model
+
+model : Model
 model =
     { content = ""
     , searchDatabase = []
     , searchIndex = searchIndex
     }
 
-type Message =
-    Search String
 
-update: Message -> Model -> (Model, Cmd Message)
+type Message
+    = Search String
+
+
+update : Message -> Model -> ( Model, Cmd Message )
 update message model =
     case message of
         Search newContent ->
-            ({ model | content = newContent }, Cmd.none)
+            ( { model | content = newContent }, Cmd.none )
 
-view: Model -> Html Message
+
+view : Model -> Html Message
 view model =
     let
-        searchResults = Result.map (\x -> List.map Tuple.first (Tuple.second x) ) (ElmTextSearch.search model.content model.searchIndex)
+        searchResults =
+            Result.map (\x -> List.map Tuple.first (Tuple.second x)) (ElmTextSearch.search model.content model.searchIndex)
     in
-    div []
-        [ input [ type_ "search" , id "searchInput", value model.content, placeholder "Search anything…" , autocomplete False, onInput Search ] []
-        , output [ ariaHidden (String.isEmpty model.content) ]
-            [ section [] [ h1 [] [ text ("Search results for “" ++ model.content ++ "”") ] ]
-            , case searchResults of
-                  Ok searchResults ->
-                      ol [ class "list--flat" ]
-                          (List.map
-                               (\searchResult ->
+        div []
+            [ input [ type_ "search", id "searchInput", value model.content, placeholder "Search anything…", autocomplete False, onInput Search ] []
+            , output [ ariaHidden (String.isEmpty model.content) ]
+                [ section [] [ h1 [] [ text ("Search results for “" ++ model.content ++ "”") ] ]
+                , case searchResults of
+                    Ok searchResults ->
+                        ol [ class "list--flat" ]
+                            (List.map
+                                (\searchResult ->
                                     let
-                                        metadata = Maybe.withDefault emptySearchMetadata (find (\l -> .id l == searchResult) model.searchDatabase)
+                                        metadata =
+                                            Maybe.withDefault emptySearchMetadata (find (\l -> .id l == searchResult) model.searchDatabase)
                                     in
                                         li []
-                                            [ a [ href ( .url metadata ) ]
-                                                [ code [] [ text ( .name metadata ) ] ]
-                                            , span [] [ text ( .description metadata ) ] ] )
-                               searchResults)
-                  _ ->
-                      p [] [ text "No result found, sorry!" ]
-            ]
-        ]
+                                            [ a [ href (.url metadata) ]
+                                                [ code [] [ text (.name metadata) ] ]
+                                            , span [] [ text (.description metadata) ]
+                                            ]
+                                )
+                                searchResults
+                            )
 
-find: (a -> Bool) -> List a -> Maybe a
+                    _ ->
+                        p [] [ text "No result found, sorry!" ]
+                ]
+            ]
+
+
+find : (a -> Bool) -> List a -> Maybe a
 find predicate list =
     case list of
         [] ->
             Nothing
 
-        first::rest ->
+        first :: rest ->
             if predicate first then
                 Just first
             else
                 find predicate rest
 
-subscriptions: Model -> Sub Message
+
+subscriptions : Model -> Sub Message
 subscriptions model =
     Sub.none
+
 
 main =
     Html.programWithFlags
--- src/Compiler/Target/Html/Elm/SearchIndexBuilder.elm
+++ src/Compiler/Target/Html/Elm/SearchIndexBuilder.elm
@@ -3,40 +3,51 @@ port module SearchIndexBuilder exposing (..)
 import Platform
 import ElmTextSearch exposing (..)
 
+
 type alias SearchIndex =
-    { id: String
-    , normalizedName: String
-    , description: String
+    { id : String
+    , normalizedName : String
+    , description : String
     }
 
-port input: (List SearchIndex -> msg) -> Sub msg
-port output: String -> Cmd msg
 
-type alias Model = ()
+port input : (List SearchIndex -> msg) -> Sub msg
 
-type Message =
-    Export (List SearchIndex)
-        
-update: Message -> Model -> (Model, Cmd Message)
+
+port output : String -> Cmd msg
+
+
+type alias Model =
+    ()
+
+
+type Message
+    = Export (List SearchIndex)
+
+
+update : Message -> Model -> ( Model, Cmd Message )
 update message model =
     case message of
         Export database ->
-            (model, output (exportIndex database))
+            ( model, output (exportIndex database) )
 
-index: ElmTextSearch.Index SearchIndex
+
+index : ElmTextSearch.Index SearchIndex
 index =
     ElmTextSearch.new
         { ref = .id
         , fields =
-            [ (.normalizedName, 3.0)
-            , (.description, 1.0)
+            [ ( .normalizedName, 3.0 )
+            , ( .description, 1.0 )
             ]
         , listFields = []
         }
 
-exportIndex: List SearchIndex -> String
+
+exportIndex : List SearchIndex -> String
 exportIndex database =
     ElmTextSearch.storeToString (Tuple.first (ElmTextSearch.addDocs database index))
 
+
 main =
-    Platform.program { init = ((), Cmd.none), subscriptions = \model -> input Export, update = update }
+    Platform.program { init = ( (), Cmd.none ), subscriptions = \model -> input Export, update = update }
