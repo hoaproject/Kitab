5b90252125c50583de8e7115d2bcf8db6a267680
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-05-30 12:41:43 +0200

    Target: Fix `yield from` keyword in Prism.

--- src/Compiler/Target/Html/Template/Public/javascript/prism-abbreviate.js
+++ src/Compiler/Target/Html/Template/Public/javascript/prism-abbreviate.js
@@ -8,7 +8,7 @@
     delete Prism.languages.php.package;
 
     // `keyword` is likely to not be up-to-date, declare ours.
-    Prism.languages.php.keyword = /\b(abstract|and|array|as|break|callable|case|catch|class|clone|const|continue|declare|default|die|do|echo|else|elseif|empty|enddeclare|endfor|endforeach|endif|endswitch|endwhile|eval|exit|extends|final|finally|for|foreach|function|global|goto|if|implements|include|include_once|instanceof|insteadof|interface|isset|list|namespace|new|or|print|private|protected|public|require|require_once|return|static|switch|throw|trait|try|unset|use|var|while|xor|yield|yieldfrom)\b/;
+    Prism.languages.php.keyword = /\b(abstract|and|array|as|break|callable|case|catch|class|clone|const|continue|declare|default|die|do|echo|else|elseif|empty|enddeclare|endfor|endforeach|endif|endswitch|endwhile|eval|exit|extends|final|finally|for|foreach|function|global|goto|if|implements|include|include_once|instanceof|insteadof|interface|isset|list|namespace|new|or|print|private|protected|public|require|require_once|return|static|switch|throw|trait|try|unset|use|var|while|xor|yield|yield from)\b/;
 
     // Introduce `native-type`.
     Prism.languages.insertBefore(
