7a954f8741e49b860c9d1dfbd774ecf1c20e9d23
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-11-13 22:02:28 +0100

    fix(configuration) Use the Kitab's logo.

--- .kitab.target.html.php
+++ .kitab.target.html.php
@@ -4,7 +4,7 @@ use Kitab\Compiler\Target\Html\Configuration;
 
 $configuration = new Configuration();
 $configuration->defaultNamespace = 'Kitab';
-$configuration->logoURL          = 'https://static.hoa-project.net/Image/Hoa.png';
+$configuration->logoURL          = 'https://raw.githubusercontent.com/hoaproject/Kitab/master/resource/logo.svg?sanitize=true';
 $configuration->projectName      = 'Kitab';
 $configuration->composerFile     = __DIR__ . '/composer.json';
 
