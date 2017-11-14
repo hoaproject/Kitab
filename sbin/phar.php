fd3e485185f75274f81ce8f9b009af0525fda979
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-11-14 11:05:04 +0100

    fix(phar) Try to debug PHAR build on Travis.

--- sbin/phar.php
+++ sbin/phar.php
@@ -32,6 +32,8 @@ $iterator = new CallbackFilterIterator(
     function ($file) use ($root) {
         $relativePath = substr($file->getPathname(), strlen($root));
 
+        var_dump($file->getPathname());
+
         return 0 === preg_match(
             '#^(' .
                 'vendor/atoum/atoum/bin|' .
