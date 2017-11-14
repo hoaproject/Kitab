82195cbe9468710db9756fe8b15946d221c41c41
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2017-11-14 10:29:44 +0100

    fix(configuration) Let Kitab find the autoloader.

--- .kitab.target.doctest.php
+++ .kitab.target.doctest.php
@@ -4,7 +4,6 @@ use Kitab\Compiler\Target\DocTest\Configuration;
 
 $configuration = new Configuration();
 
-$configuration->autoloaderFile      = __DIR__ . '/vendor/autoload.php';
 $configuration->concurrentProcesses = 4;
 
 return $configuration;
